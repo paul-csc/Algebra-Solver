e5c1078cb706da080470c3d350404e831f94c859
paul-csc <paulchuxin.liu@outlook.com>
2026-01-25 13:00:20 -0500

    Add constants

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,23 @@
 #include "Solver.h"
+#include <iomanip>
 #include <iostream>
+#include <sstream>
+
+static std::string FormatDouble(double x, int precision = 6) {
+    std::ostringstream oss;
+    oss << std::fixed << std::setprecision(precision) << x;
+
+    std::string s = oss.str();
+    s.erase(s.find_last_not_of('0') + 1);
+    if (!s.empty() && s.back() == '.') {
+        s.pop_back();
+    }
+
+    return s;
+}
 
 int main() {
-    const std::string_view equation = "x = asin(1)";
+    const std::string_view equation = "x = 3pi";
     const auto solutions = Solve(equation);
 
     if (solutions.IsNone) {
@@ -11,7 +26,7 @@ int main() {
         std::cout << "Infinite solutions\n";
     } else {
         for (double solution : solutions.Values) {
-            std::cout << solution << "\n";
+            std::cout << FormatDouble(solution, 10) << "\n";
         }
     }
     
